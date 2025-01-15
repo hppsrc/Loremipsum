66cd0e1ebd9af3647fe17bd68f5300d4bcd57572
hppsrc <horahenaripo+new@gmail.com>
2025-01-14 19:48:30 -0500

    0.2.0-alpha
    - Now you can modify the string that is taken as file format and reference for the detection process.
    - Implemented a function to process the text and one to check if the file is valid.
    - linter.exe can now interpret a .loremipsum file, but only when it is a single argument, commands will be implemented soon.
    - Added warning notice.
    - Added a (temporary) icon to the program.
    - Simple code formatting.

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 Loremipsum is an “interpreter” made on C that converts a string “Loremipsum” to an ASCII character.
 
-Current version: **0.1.0-alpha**
+Current version: **0.2.0-alpha**
 
 > [!WARNING]
 > Loremipsum is currently in ***ALPHA***, much of what is said here may not be implemented yet. ***Be careful!***
@@ -16,7 +16,7 @@ Current version: **0.1.0-alpha**
 
 ### Dependencies 🗃️
 
-- [CLRSTR.h](https://github.com/hppsrc/libs/blob/main/C/CLRSTR.h) (Build 25011213)
+- [CLRSTR.h](https://github.com/hppsrc/libs/blob/main/C/CLRSTR.h) (Build 25011316)
 
 ### Build yourself 🛠️
 
@@ -24,25 +24,25 @@ Current version: **0.1.0-alpha**
 
 Using clang 19.1.4 (via MSYS2)
 
-- On your console use:
+- Compile resources:
 
-        clang .\src\main.c
+        windres .\src\res\res.rc -O coff -o app.res
+
+- Compile source code:
+
+        clang .\src\main.c app.res
 
 ### Download ⬇️
 
-Download last binaries here.
+Download last binaries for:
 
-<!-- - Last version for
-  - Windows: ***0.1.0-alpha*** -->
+- [Windows (x64)](https://github.com/hppsrc/Loremipsum/releases/download/0.2.0-alpha/linter_x86_64.exe).
 
 ### Installing ⚙️
 
 *No installation required.*
 
 ### Using Loremipsum 💻
-
-> [!WARNING]
-> Currently the code doesn't execute any action.
 
 - You must pass as argument a file of the type `.loremipsum`.
 
@@ -85,12 +85,21 @@ This project is licensed under the GNU Lesser General Public License v3.0 (LGPLv
 
 - [ ] Ensure compatibility with Windows and Linux.
 - [ ] Arch Linux build.
+- [ ] Proper code documentation.
 
 ## Known Errors 🐞
 
 None found!
 
 ## Version History 🕒
+
+- 0.2.0-alpha
+  - Now you can modify the string that is taken as file format and reference for the detection process.
+  - Implemented a function to process the text and one to check if the file is valid.
+  - linter.exe can now interpret a .loremipsum file, but only when it is a single argument, commands will be implemented soon.
+  - Added warning notice.
+  - Added a (temporary) icon to the program.
+  - Simple code formatting.
 
 - 0.1.0-alpha
   - Initial Release.
--- src/main.c
+++ src/main.c
@@ -6,26 +6,156 @@
 // * Source code on: https://github.com/hppsrc/loremipsum
 // * Under LGPLv3 license feel free to modify, distribute and use this code for any purpose.
 
-#define VERSION "0.1.0-alpha"
-#define BUILD "250112151"
-#define STRING "Loremipsum"
+#define VERSION "0.2.0-alpha"
+#define BUILD "250114191"
+#define STRING "LOREMIPSUM" // String has to be uppercase
 
 #define ERROR printf_clrstr("error:", RED, NONE, END)
+#define WARNING printf_clrstr("warning:", YELLOW, NONE, END)
+
+#define ICON "myicon.ico"
 
 #include <stdio.h>
+#include <ctype.h>
 #include <stdlib.h>
 
 #include "lib/CLRSTR.h"
 
-// TODO file check action
+char _STRING_[255];
+char _LOW_STRING_[255];
+
+char* run_file(char file_name[]) {
+
+    FILE *file;
+    char content[255];
+    int arr[255];
+    for (size_t i = 0; i < 255; i++) { arr[i] = 0; }
+
+    char *token = strtok(file_name, ".");
+    char *last_token = token;
+
+    while (token != NULL) {
+        last_token = token;
+        token = strtok(NULL, ".");
+    }
+
+    for(int i = 0; last_token[i]; i++){ last_token[i] = tolower(last_token[i]); }
+    
+    if ( !strcmp(last_token, _LOW_STRING_ ) ) {
+
+        sprintf(file_name, "%s.%s", file_name, _LOW_STRING_);
+        
+        file = fopen(file_name, "r");
+
+        char temp[11];
+        int index = 0;
+        int Arri = 0;
+        arr[Arri] = 0;
+
+        while (fgets(content, sizeof(content), file) != NULL) {
+
+            for (int i = 0; content[i] != '\0'; i++) {
+
+                if ( !strcmp(temp, _LOW_STRING_) ) {
+
+                    index = 0;
+                    arr[Arri]++;
+                    memset(temp, 0, sizeof(temp));
+
+                } else if ( !strcmp(temp, _STRING_) ) {
+
+                    index = 0;
+                    arr[Arri]+=32;
+                    memset(temp, 0, sizeof(temp));
+
+                } else {
+
+                    if (content[i] != '\n') {
+                        temp[index] = content[i] ;
+                    }
+                    index++;
+
+                }
+
+                // printf("CHAR ACTUAL: \"%c\"\n",content[i]);
+                // printf("TEMP ACTUAL: \"%s\"\n",temp);
+                // printf("INDEX ACTUAL: %d\n",index);
+                // printf("BUFFER ACTUAL: %d\n", Arri);
+                // for (size_t i = 0; i < 5; i++) {
+                //     printf("buffer %zu: %d\n", i, arr[i]);
+                // }
+
+                if (content[i] == '\n') {
+                    Arri++;
+                    index--;
+                }
+                
+            }
+
+        }
+
+        fclose(file);
+
+    }
+    
+    int i = 0;
+    char result[255];
+    int pos = 0;
+    while (arr[i] != 0) {
+        result[pos++] = (char)arr[i];
+        i++;
+    }
+    result[pos] = '\0';
+
+    return strdup(result);
+
+}
+
+int file_check(char file_name[]) {
+
+    FILE *file;
+    char content[255];
+    int arr[255];
+    for (size_t i = 0; i < 255; i++) { arr[i] = 0; }
+
+    char *token = strtok(file_name, ".");
+    char *last_token = token;
+
+    while (token != NULL) {
+        last_token = token;
+        token = strtok(NULL, ".");
+    }
+
+    for(int i = 0; last_token[i]; i++){ last_token[i] = tolower(last_token[i]); }
+
+    if ( !strcmp(last_token, _LOW_STRING_) ) {
+
+        sprintf(file_name, "%s.%s", file_name, _LOW_STRING_);
+        file = fopen(file_name, "r");
+
+        if (file == NULL) {
+            printf( "Loremipsum: %s Unable to open or doesn't exist\n", printf_clrstr( "error:", RED, NONE, END));
+            return 1;
+        }
+
+    } else {
+        printf( "Loremipsum: %s file '%s' not allowed\n", printf_clrstr( "error:", RED, NONE, END), printf_clrstr( file_name, CYAN, NONE, END) );
+        return 1;
+    }
+
+    return 0;
+
+}
 
 int main(int argc, char *argv[]) {
+
+    strcpy(_STRING_, STRING);
+    for(int i = 0; _STRING_[i]; i++){ _LOW_STRING_[i] = tolower(_STRING_[i]); }
 
     argc--;
 
     int o_flag = 0;
     int no_cap_flag = 0;
-    int intr_flag = 0;
 
     if( argc == 0 ) {
 
@@ -74,17 +204,21 @@ int main(int argc, char *argv[]) {
             } else {
 
                 printf( "Loremipsum: %s Unknown command '%s'\n", ERROR,
-                                                                    printf_clrstr(argv[1], CYAN, NONE, END) );
+                                                                 printf_clrstr(argv[1], CYAN, NONE, END) );
                 
             }
 
-        } 
+        } else {
 
-        // TODO is file .loremipsum file? if so, set intr to 1, else -1
+            if (file_check(argv[1]) == 0) {
+                
+                printf("%s",run_file(argv[1]));
+                
+            }
+
+        }
 
     } else if ( argc == 2 ) {
-
-        // TODO add 1st arg func check
 
         if (!strcmp(argv[1], "--o") || !strcmp(argv[1], "--no_cap")) {
 
@@ -93,20 +227,24 @@ int main(int argc, char *argv[]) {
                 if (!strcmp(argv[i], "--no_cap")) { no_cap_flag = 1; }
             }
 
-            if ( (o_flag + no_cap_flag == 1) && strcmp(argv[1], "") ) {
+            if ( (o_flag + no_cap_flag == 1) && file_check(argv[2]) == 0 ) {
+
+                // create txt file
+                if (o_flag==1) { printf("%s",run_file(argv[2])); }
+
+                // create temp file lowercased
+                else { printf("%s",run_file(argv[2])); }
+
+            } else if ( (o_flag + no_cap_flag == 1) && strcmp(argv[1], "") ) {
 
                 printf("Loremipsum: %s Unexpected additional argument: '%s'\n", ERROR,
                                                                                 printf_clrstr(argv[2], CYAN, NONE, END));
 
             } else if ( o_flag == 1 && no_cap_flag == 1 ) {
                 printf( "Loremipsum: %s file expected with '%s' and '%s' \n", ERROR,
-                                                                         printf_clrstr(argv[1], CYAN, NONE, END),
-                                                                         printf_clrstr(argv[2], CYAN, NONE, END));
-            } else if ( o_flag == 1 ) {
-                // TODO is file .loremipsum file? if so, set intr to 1, else -1
-            } else if ( no_cap_flag == 1 ) {
-                // TODO is file .loremipsum file? if so, set intr to 1, else -1
-            }
+                                                                              printf_clrstr(argv[1], CYAN, NONE, END),
+                                                                              printf_clrstr(argv[2], CYAN, NONE, END));
+            } 
 
         }  else {
 
@@ -117,26 +255,26 @@ int main(int argc, char *argv[]) {
 
     } else if ( argc == 3 ) {
 
-        if (!strcmp(argv[1], "--o") || !strcmp(argv[1], "--no_cap")) {
+        printf("Loremipsum: %s Not implemented yet\n", WARNING);
 
-            for (int i = 1; i <= argc; i++) {
-                if (!strcmp(argv[i], "--o")) { o_flag = 1; }
-                if (!strcmp(argv[i], "--no_cap")) { no_cap_flag = 1; }
-            }
+        // if (!strcmp(argv[1], "--o") || !strcmp(argv[1], "--no_cap")) {
+
+        //     for (int i = 1; i <= argc; i++) {
+        //         if (!strcmp(argv[i], "--o")) { o_flag = 1; }
+        //         if (!strcmp(argv[i], "--no_cap")) { no_cap_flag = 1; }
+        //     }
             
-            // if flag = 2 and file = good
-            // if flag = 2 and not file = error
-            // if 
+        //     // if flag = 2 and file = good
+        //     // if flag = 2 and not file = error
+        //     // if 
 
-        }
+        // }
 
     }  else {
 
         printf("Loremipsum: %s Too many arguments\n", ERROR);
 
     }
-
-    // TODO if intr = 1 start action, else error
 
     return 0;
 
